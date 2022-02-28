67ea60c92dd4a4d2286b8ee7ce89b45fe45a8f01
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-02-28 14:22:10 -0600

    more redux changes

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,15 @@ import { createAppContainer } from 'react-navigation';
 import Constants from 'expo-constants';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+	fetchCampsites,
+	fetchComments,
+	fetchPromotions,
+	fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
 	{
@@ -174,6 +183,13 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+	componentDidMount() {
+		this.props.fetchCampsites();
+		this.props.fetchComments();
+		this.props.fetchPromotions();
+		this.props.fetchPartners();
+	}
+
 	render() {
 		return (
 			<View
@@ -217,4 +233,4 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
